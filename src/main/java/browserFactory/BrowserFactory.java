bf129b03fb8c3fed081a7986fbf10c7a45a2373b
sethupriyadharshini <57996770+sethupriyadharshini@users.noreply.github.com>
2023-04-09 14:16:34 +0530

    Update BrowserFactory.java

--- src/main/java/browserFactory/BrowserFactory.java
+++ src/main/java/browserFactory/BrowserFactory.java
@@ -26,7 +26,6 @@ public class BrowserFactory {
 		if(browserName.contains("Chrome") || browserName.contains("GC") || browserName.contains("Google Chrome"))
 		{
 			ChromeOptions opt=new ChromeOptions();
-			driver=new ChromeDriver();
 			opt.addArguments("--headless");
 			opt.addArguments("--no-sandbox");
 			driver=new ChromeDriver(opt);
